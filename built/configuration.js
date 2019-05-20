f79bd1033d093019eda6a4cce60cd79582414121
Josef Wittmann <josefwittmann42@gmail.com>
2019-05-21 00:55:58 +0200

    Commented configurations and added actOnAllConfigurations

--- built/configuration.js
+++ built/configuration.js
@@ -1,12 +1,31 @@
+// representation of a valid mine configuration/distribution
+// each instance stores a coherent part of a complete configuration and links to its successors
+// by traversing the resulting tree structure, all distributions can be obtainend
 class Configuration {
-    constructor(subConfigurations, cellValues) {
+    constructor(subConfigurations, 
+    // cell values in this part of a complete configuration
+    // cells may only occur once in a path in the configuration tree
+    cellValues) {
         this.subConfigurations = subConfigurations;
         this.cellValues = cellValues;
     }
+    // indicates that there is no further configuration
     isLast() {
         return this.subConfigurations.length == 0;
     }
+    // iterate over subConfigurations
     [Symbol.iterator]() {
         return this.subConfigurations.values();
+    }
+    // perform callback on every possible complete configuration
+    actOnAllConfigurations(callback, allCellValues = new Map()) {
+        this.cellValues.forEach((value, key) => allCellValues.set(key, value));
+        if (this.isLast()) {
+            callback(allCellValues);
+            return;
+        }
+        for (let subConfiguration of this.subConfigurations) {
+            subConfiguration.actOnAllConfigurations(callback, allCellValues);
+        }
     }
 }
--- src/configuration.ts
+++ src/configuration.ts
@@ -1,17 +1,31 @@
+// representation of a valid mine configuration/distribution
+// each instance stores a coherent part of a complete configuration and links to its successors
+// by traversing the resulting tree structure, all distributions can be obtainend
 class Configuration {
-  subConfigurations: Configuration[];
-  cellValues: Map<number, number>;
+  constructor(public subConfigurations: Configuration[],
+    // cell values in this part of a complete configuration
+    // cells may only occur once in a path in the configuration tree
+    public cellValues: Map<number, number>) {}
 
-  constructor(subConfigurations: Configuration[], cellValues: Map<number, number>) {
-    this.subConfigurations = subConfigurations;
-    this.cellValues = cellValues;
-  }
-
+  // indicates that there is no further configuration
   isLast() {
     return this.subConfigurations.length == 0;
   }
 
+  // iterate over subConfigurations
   [Symbol.iterator]() {
     return this.subConfigurations.values();
+  }
+
+  // perform callback on every possible complete configuration
+  actOnAllConfigurations(callback: (allCellValues: Map<number, number>) => any, allCellValues: Map<number, number> = new Map()) {
+    this.cellValues.forEach((value, key) => allCellValues.set(key, value));
+    if(this.isLast()) {
+      callback(allCellValues);
+      return;
+    }
+    for(let subConfiguration of this.subConfigurations) {
+      subConfiguration.actOnAllConfigurations(callback, allCellValues);
+    }
   }
 }
